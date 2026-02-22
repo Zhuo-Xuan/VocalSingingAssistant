6104ececdaaca657e51e994978c2808da3648be7
Zhuo-Xuan <167042423+Zhuo-Xuan@users.noreply.github.com>
2026-02-22 06:35:46 -0800

    Delete AdditionalTools directory

--- AdditionalTools/start_frontend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting frontend server...
-echo.
-echo Open your browser and go to: http://localhost:8080
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-cd frontend
-python -m http.server 8080
